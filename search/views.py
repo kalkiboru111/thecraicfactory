2856ce73856909f0898d42762159e34e778a52d8
Ubuntu <ubuntu@ip-172-31-12-47.eu-west-1.compute.internal>
2019-09-05 17:11:44 +0000

    Fixed the search function

--- search/views.py
+++ search/views.py
@@ -3,6 +3,6 @@ from posts.models import Post
 
 # Create your views here.
 def do_search(request):
-    products = Post.objects.filter(name__icontains=request.GET['q'])
+    posts = Post.objects.filter(content__icontains=request.GET['q'])
     return render(request, "posts.html", {"posts": posts})
     
