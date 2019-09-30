4a83ce1eb2336a59c25f997d157e451186598a87
Ubuntu <ubuntu@ip-172-31-12-47.eu-west-1.compute.internal>
2019-09-30 08:26:44 +0000

    File paths for images

--- accounts/models.py
+++ accounts/models.py
@@ -9,20 +9,11 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     # set default profile photo and specifies directory where images will be uplaoded to.
-    profile_image = models.ImageField(default='profile.jpg', upload_to='profile_pics') # where is stored the user profile image
+    profile_image = models.ImageField(default='/media/post_images/profile.jpeg', upload_to='profile_pics') # where is stored the user profile image
     
     def __str__(self):
         return f'{self.user.username} Profile'
-    
-    def save(self, *args, **kwargs):
-        super.save(*args, **kwargs)
-        
-        img = Image.open(self.image.path)
-        
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+   
             
     
     
