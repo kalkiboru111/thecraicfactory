1391d0715b270ec13649bebf08bee1e22ae0ffaa
Ubuntu <ubuntu@ip-172-31-12-47.eu-west-1.compute.internal>
2019-10-01 12:52:35 +0000

    Latest commit

--- thecraicfactory/settings.py
+++ thecraicfactory/settings.py
@@ -168,7 +168,7 @@ MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILES_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'thecraicfactory/media')
 
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
