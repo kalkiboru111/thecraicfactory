5f281d0fb8270f621302f5f0aee39d27191513a1
Ubuntu <ubuntu@ip-172-31-27-126.eu-west-1.compute.internal>
2019-10-11 08:29:16 +0000

    Removed local media folder

--- posts/models.py
+++ posts/models.py
@@ -15,7 +15,6 @@ class Post(models.Model):
     friend_tag = models.CharField(max_length=30, blank=True, null=True)
     image = models.ImageField(upload_to="post_images/", blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts_created')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __unicode__(self):
         return self.title
