b9c23d1f0034b1d5a5c7193f5eecb76c3b1331bc
Ubuntu <ubuntu@ip-172-31-27-126.eu-west-1.compute.internal>
2019-10-15 12:19:56 +0000

    new test

--- env.py
+++ env.py
@@ -5,5 +5,5 @@ os.environ.setdefault("STRIPE_SECRET", "sk_test_WYrIt48U5ZI7EkRbajycf0HN00p9RMdI
 
 os.environ.setdefault("DATABASE_URL", "postgres://zkiaaxucxnhjio:3eaf8f397c6b33cafa2a36f9ff0fd45505f042c5fd270be84c6068a6a73ad4d6@ec2-176-34-183-20.eu-west-1.compute.amazonaws.com:5432/daucd8a4maisni")
 os.environ.setdefault("SECRET_KEY", "3q48(f6q1umu16u+w0&8stofzx^2c!mjc7)7=y%17$w^pin-gk")
-os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIA5UPDGANY7A6XMSXQ")
-os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIA5UPDGANYVRMS6BXV")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
