d374dd8313bb6c896bbb677ad346c66cc0115552
Ubuntu <ubuntu@ip-172-31-27-126.eu-west-1.compute.internal>
2019-10-24 07:55:49 +0000

    Updated cart functionality to remove ids passing

--- cart/views.py
+++ cart/views.py
@@ -1,35 +1,69 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from posts.models import Post
-from posts.models import Product, PostProduct
+from posts.models import Product
+from django.http import HttpResponseRedirect
+
+@login_required
+def add_to_cart(request):
+    product_id = request.POST['product']
+    quantity = int(request.POST.get('quantity'))
+    post = request.POST['post']
+
+    products = Product.objects.all()
+    cart = request.session.get('cart', {})
+    
+    if product_id in cart:
+        if post in cart[product_id]:
+            cart[product_id][post] += quantity
+        else:
+            cart[product_id].update({post:quantity})
+    else:
+        cart[product_id]={post:quantity}
+        
+    print(cart) 
+    
+    #cart[id] = cart.get(id, quantity, pk)
+
+    request.session['cart'] = cart
+    #return render(request, "cart2.html", {"post": post, "products": products})
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 # Create your views here.
 @login_required
 def view_cart(request):
-    post_product = get_object_or_404(PostProduct)
-    return render(request, "cart2.html", {"post_product": post_product} )
-
-@login_required
-def add_to_cart(request, id, pk):
-    quantity = int(request.POST.get('quantity'))
-    post = get_object_or_404(Post, pk=pk)
-    print(post.id)
-
     cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
-
-    request.session['cart'] = cart
-    return render(request, "cart2.html", {"post": post})
-
-@login_required
-def adjust_cart(request, id):
-    quantity = int(request.POST.get('quantity'))
-    cart = request.session.get('cart', {})
-
-    if quantity > 0:
-        cart[id] = quantity
-    else:
-        cart.pop(id)
+    cart_items = []
+    cart_total =0
     
+    for product_id, post in cart.items():
+        product = get_object_or_404(Product, pk=product_id)
+        for post, quantity in post.items():
+            
+            cart_items.append({
+                'id': product.id,
+                'name': product.name,
+                'post': post,
+                'description': product.description,
+                'quantity': quantity,
+                'total': product.price * quantity,
+                })  
+            
+            cart_total += product.price * quantity
+    
+    return render(request, "cart2.html", {'cart_items': cart_items, 'cart_total': cart_total})
+
+
+@login_required
+def adjust_cart(request):
+    product_id = request.POST['product']
+    post = request.POST['post']
+    cart = request.session.get('cart', {})
+    del cart[product_id][post]
     request.session['cart'] = cart
+    # if quantity > 0:
+    #     cart[id] = quantity
+    # else:
+    #     cart.pop(id)
+    #request.session['cart'] = cart
     render(request, "cart2.html")
--- products/views.py
+++ products/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
-from posts.models import Post, PostProduct
+from posts.models import Post
 
 # Create your views here.
 def all_products(request, pk):
     products = Product.objects.all()
     post = get_object_or_404(Post, pk=pk)
-    post_product = get_object_or_404(PostProduct, pk=pk)
-    print(post.id)
-    print(post_product.id)
-    return render(request, "products.html", {"products": products, "post": post, "post_product": post_product})
+    return render(request, "products.html", {"products": products, "post": post})
