053ca8a1c694c99dba46828bdb73d57d5352d06c
Ubuntu <ubuntu@ip-172-31-27-126.eu-west-1.compute.internal>
2019-10-25 10:01:23 +0000

    commented out env file

--- thecraicfactory/settings.py
+++ thecraicfactory/settings.py
@@ -16,10 +16,10 @@ import dj_database_url
 #if os.path.exists('env.py'):
     #import env
     
-try:
-    import env
-except:
-    pass   
+# try:
+#     import env
+# except:
+#     pass   
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
